8fb1db52424aa71ae12218452baf20cb78c4c7dc
wuling31715 <wuling31715@gmail.com>
2017-09-09 15:44:32 +0800

    upload

--- bot/module/city_url.py
+++ bot/module/city_url.py
@@ -2,7 +2,7 @@ import csv
 
 class Pixnet:
     def get_url(city_to_index):
-        with open('./pixnet.csv', 'r') as csvfile:
+        with open('city_url.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 if row['city'] == city_to_index:
